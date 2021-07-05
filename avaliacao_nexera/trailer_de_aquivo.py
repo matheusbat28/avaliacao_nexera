923f795be0d25381996850ee5f1cd69aa3f0c663
MATHEUS BATISTA <78868039+matheusbat28@users.noreply.github.com>
2021-07-05 15:44:01 -0300

    Delete avaliacao_nexera directory

--- avaliacao_nexera/trailer_de_aquivo.py
+++
@@ -1,7 +0,0 @@
-class Trailer_de_arquivo:
-
-    def __init__(self, linha):
-         self.linha = linha
-
-    def __str__(self):
-        return self.linha
--- avaliacao_nexera/trailer_de_lote.py
+++
@@ -1,7 +0,0 @@
-class Trailer_de_lote:
-
-    def __init__(self, linha):
-        self.linha = linha
-
-    def __str__(self):
-        return self.linha
